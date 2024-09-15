25b4828ba239071f74dbc97ea13e027abf0ab9ef
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-15 11:08:28 +0300

    Summary

---
+++ src/app/components/header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header className="min-h-28 flex items-center justify-center bg-gray-600">
+      <nav className="flex items-center justify-center gap-5">
+        <Link
+          href="/"
+          className={`link ${
+            pathname === "/" ? "text-amber-400" : "text-white"
+          }`}
+        >
+          Home page
+        </Link>
+        <Link
+          href="/about"
+          className={`link ${
+            pathname === "/about" ? "text-amber-400" : "text-white"
+          }`}
+        >
+          About page
+        </Link>
+      </nav>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="mt-24 font-semibold">My Home Page</h1>;
+  return (
+    <>
+      <h1 className="mt-24 font-semibold">My Home Page</h1>
+    </>
+  );
 }
