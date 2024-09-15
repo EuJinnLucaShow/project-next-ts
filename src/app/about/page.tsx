0c3928697e4530145560b67e34589d565c1b44d2
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-15 20:10:33 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function About() {
+  const router = useRouter();
+
   return (
     <div>
       <h2>About page</h2>
+      <button type="button" onClick={() => router.push("/")}>
+        Back
+      </button>
     </div>
   );
 }
