31d1d6f52aafa2a99f5a9fe1d565dc072ac04033
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-15 21:35:48 +0300

    Summary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import SocLinks from "./ui/SocLinks";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center gap-10">
       <h1 className="mt-11 text-2xl">Home Page</h1>
+      <SocLinks />
     </div>
   );
 }
